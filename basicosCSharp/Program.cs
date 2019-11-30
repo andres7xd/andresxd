ee250ac7334c0aa7f503cd7c19f6dbe37e36a5e6
andres7xd <58347580+andres7xd@users.noreply.github.com>
2019-11-30 10:01:52 -0600

    Se trabaja el ciclo For C#

--- basicosCSharp/Program.cs
+++ basicosCSharp/Program.cs
@@ -80,6 +80,13 @@ namespace basicosCSharp
                 rondasFaltantes--;
             } while (rondasFaltantes > 0);
 
+            //For
+            string[] diasSemanales = { "Lunes", "Martes", "Miércoles", "Jueves", "Viernes", "Sábado", "Domingo" };
+            for (int dia = 0; dia < 7; dia++)
+            {
+                Console.WriteLine("{0} es el número {1} de la semana", diasSemanales[dia], dia + 1);
+            }
+         
 
 
 
